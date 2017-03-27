4282d6d9ead2de13e12287bd1b4be3cd0775dfa5
Mads Lentoft <madslentoft@gmail.com>
2017-03-27 12:58:54 +0200

    Added more styling and images

--- menu_preview/2/js/scripts.js
+++ menu_preview/2/js/scripts.js
@@ -1,1 +1,14 @@
 // BOX-stroids website functions
+$(document).ready(function() {
+	function realigntext() {
+		$('.segment').each(function() {
+			this.style.lineHeight = $(this).height() + "px";
+		});
+		var count = $("#menu ul").children().length;
+		$('#menu ul').css('line-height', ($('#menu ul').height() / ((3/2) * count)) + 'px')
+	}
+	realigntext();
+	$(window).resize(function () {
+		realigntext();
+	});
+});
