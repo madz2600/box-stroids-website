daf67a3a6c6d4fbb6fb2b6f8ccf90132e00af480
Mads Lentoft <madslentoft@gmail.com>
2017-03-27 13:47:43 +0200

    Added sounds and button styling

--- menu_preview/2/js/scripts.js
+++ menu_preview/2/js/scripts.js
@@ -10,5 +10,15 @@ $(document).ready(function() {
 	realigntext();
 	$(window).resize(function () {
 		realigntext();
+	});	
+	$("#menu ul li").each(function(i) {
+		if (i != 0) {
+			$("#beep-two").clone().attr("id", "beep-two" + i).appendTo($(this).parent());
+		}
+		$(this).data("beeper", i);
+	  })
+	.mouseenter(function() {
+		$("#beep-two" + $(this).data("beeper"))[0].play();
 	});
+	$("#beep-two").attr("id", "beep-two0");
 });
